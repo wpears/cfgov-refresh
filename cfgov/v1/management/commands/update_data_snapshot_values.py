2a4041f7b28a32b6e86dad131e7cf83e3a6476c5
Richa Agarwal <richa.agarwal85@gmail.com>
2017-06-28 12:23:21 -0700

    wip

--- cfgov/v1/management/commands/update_data_snapshot_values.py
+++ cfgov/v1/management/commands/update_data_snapshot_values.py
@@ -1,13 +1,12 @@
 from django.core.management.base import BaseCommand
 
-from v1.atomic_elements.organisms import DataSnapshot
-from v1.models.base import CFGOVPage
-
+from v1.models.browse_page import BrowsePage
+from v1.tests.wagtail_pages.helpers import publish_changes
 
 class Command(BaseCommand):
     help = 'Monthly updates to data snapshot values'
     # Test/example command:
-    # python cfgov/manage.py update_data_snapshot_values --data_month='July 2017' --market_key='AUT' --num_originations='12345' --value_originations='1245' --year_over_year_change='123'
+    # python cfgov/manage.py update_data_snapshot_values --data_month='September 2017' --market_key='AUT' --num_originations='12345' --value_originations='1245' --year_over_year_change='123'
 
     def add_arguments(self, parser):
         """Adds all arguments to be processed."""
@@ -37,63 +36,42 @@ class Command(BaseCommand):
             help='Year over year percentage change: positive value followed by "increase" or "decrease"'
         )
 
-    def handle(self, *args, **options):
-        # Look up which market to update
-        market_key = options['market_key']
-        snapshot = self.get_data_snapshot(market_key)
+    def get_data_snapshots(self):
+        """ Gets all data snapshots
+        Assumes there is one data snapshot per page
+        """
+
+        snapshots = []
+
+        for page in BrowsePage.objects.all():
+            stream_data = page.specific.content.stream_data
+            snapshot = filter(lambda item: item['type'] == 'data_snapshot', stream_data)
+            if snapshot:
+                snapshot[0]['value']['page'] = page
+                snapshots.append(snapshot[0]['value'])
         
-        # Update fields with the provided values
+        return snapshots
+
+    def find_data_snapshot(self, market_key):
+        snapshots = self.get_data_snapshots()
+        snapshot = filter(lambda item: item['market_key'] == market_key, snapshots)
+        if snapshot:
+            return snapshot[0]
+
+
+    def handle(self, *args, **options):
+        # Look up data snapshot by the provided market key
+        snapshot = self.find_data_snapshot(options['market_key'])
+        
+        # Update snapshot fields with the provided values
         snapshot['data_month'] = options['data_month']
         snapshot['num_originations'] = options['num_originations']
         snapshot['value_originations'] = options['value_originations']
         snapshot['year_over_year_change'] = options['year_over_year_change']
 
-        # Save the updates
-        self.update_data_snapshot(snapshot)
-
-    def get_data_snapshot(self, market_key):
-        """ Get the data_snapshot object that matches the given market_key.
-        Note: This assumes there is only one matching object.
-        Returns a dictionary of the matching object's values."""
-
-        snapshots = []
-
-        # Parse through pages to find data_snapshot items
-        # This is slow but is only run once per month
-        for page in CFGOVPage.objects.all():
-            try:
-                stream_data = page.specific.content.stream_data
-            except:
-                continue
-
-            page_snapshot = list(
-                filter(lambda item: item['type'] == 'data_snapshot', stream_data))
-
-            if len(page_snapshot) > 0:
-                for item in page_snapshot:
-                    # Save the page object into the dict so we can find it again
-                    item['value']['page_obj'] = page
-                    snapshots.append(item['value'])
-        
-        # Find the matching market_key
-        snapshot = filter(lambda item: item['market_key'] == market_key, snapshots)
-
-        if len(snapshot) == 1:
-            return snapshot[0]
-        elif len(snapshot) > 1:
-            # Select first if multiple objects found
-            # TODO: Handle more appropriately (warning or error)
-            return snapshot[0]
-        else:
-            return []
-
-    def update_data_snapshot(self, new_snapshot):
-        """Save updates to the data_snapshot"""
-        page = new_snapshot['page_obj']
-        # page.specific.content.stream_data['data_snapshot'] = new_snapshot
-        page.save()
-
-
+        # Publish changes to the browse page the data snapshot lives on
+        page = snapshot['page']
+        publish_changes(page.specific)
 
 
 
